534b1453c49777eb0fd9284c2c8fe913111b9592
Sven Jansen <sven.jansen@devicetrust.com>
2023-01-03 14:28:53 +0100

    .

--- variables.tf
+++ variables.tf
@@ -1,65 +1,11 @@
 #####
 # Variables for administrative connection to the ADC
 #####
-
-variable adc-base{
-}
+variable adc-base{}
 
 #####
 # ADC GW vServer
 #####
-
-variable "adc-gw-vserver" {
-}
-
-#####
-# ADC GW vServer STA Bindings
-#####
-
-variable "adc-gw-vserver-staserverbinding" {
-}
-
-#####
-# Session Action Variables
-#####
-
-variable "adc-gw-vpnsessionaction-receiver" {
-}
-
-variable "adc-gw-vpnsessionaction-receiverweb" {
-}
-
-#####
-# Session Policiy Variables
-#####
-
-variable "adc-gw-vpnsessionpolicy" {
-}
-
-#####
-# ADC GW vServer VPN Session Policy Bindings
-#####
-
-variable "adc-gw-vserver-vpnsessionpolicybinding" {
-}
-
-#####
-# ADC Authentication LDAP Action
-#####
-
-variable "adc-gw-vserver-authenticationldapaction" {
-}
-
-#####
-# ADC Authentication LDAP Policy
-#####
-
-variable "adc-gw-vserver-authenticationldappolicy" {
-}
-
-#####
-# ADC Authentication LDAP Policy GW vServer Binding
-#####
-
-variable "adc-gw-vserver-authenticationldappolicy_binding" {
-}
+variable "adc-gw" {}
+variable "adc-gw-authenticationldapaction" {}
+variable "adc-gw-authenticationldappolicy" {}
